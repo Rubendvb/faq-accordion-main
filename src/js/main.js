99ea1d24b5edaaaa7860cc65da0432474babb548
Rubendvb <rubenvasquez556@gmail.com>
2024-07-03 00:50:35 -0300

    finish project

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+let acc = document.querySelectorAll('.accordion')
+let panels = document.querySelectorAll('.panel')
+let imgs = document.querySelectorAll('.img__plus')
+
+acc.forEach((btn, index) => {
+  btn.addEventListener('click', (e) => {
+    const btn = e.currentTarget
+
+    btn.classList.toggle('active')
+
+    if (panels[index].style.maxHeight) {
+      panels[index].style.maxHeight = null
+      panels[index].style.marginTop = '0'
+      imgs[index].setAttribute('src', './src/assets/images/icon-plus.svg')
+    } else {
+      panels[index].style.maxHeight = panels[index].scrollHeight + 'px'
+      panels[index].style.marginTop = '28px'
+      imgs[index].setAttribute('src', './src/assets/images/icon-minus.svg')
+    }
+  })
+})
